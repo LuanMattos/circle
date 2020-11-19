b3ab522ddbaa128ca3e28e633cb8c6b4a18bc722
luanmattos <patrickluan.matos@gmail.com>
2020-11-19 17:08:51 -0300

    avatar search component

--- src/app/photos/photo-list/search/search.component.ts
+++ src/app/photos/photo-list/search/search.component.ts
@@ -1,51 +1,55 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 import {PhotoService} from '../../photo/photo.service';
 import {debounceTime} from 'rxjs/operators';
 import {Router} from '@angular/router';
 import {User} from '../../../core/user/user';
+import {environment} from '../../../../environments/environment';
+import {SecurityCommonsService} from '../../../shared/services/security-commons.service';
 
 @Component({
-  selector:'app-search',
-  templateUrl:'./search.component.html',
-  styleUrls:['./search.component.css']
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
 })
-export class SearchComponent{
-  filter:string = '';
-  users:User[] = [];
-  hasMore:boolean = true;
-  _openInputSearch:boolean = false
-  @Output() openInputSearch :EventEmitter<boolean> =  new EventEmitter<boolean>();
+export class SearchComponent implements OnInit{
+  filter: string = '';
+  users: User[] = [];
+  hasMore: boolean = true;
+  _openInputSearch: boolean = false;
+  avatarDefault: string = environment.ApiUrl + 'storage/profile_default/default.png';
+  @Output() openInputSearch: EventEmitter<boolean> =  new EventEmitter<boolean>();
 
   constructor(
-    private route:Router,
-    private photoService:PhotoService ) {}
+    private securityCommons: SecurityCommonsService,
+    private route: Router,
+    private photoService: PhotoService ) {}
 
 
-  _filter( value:string ){
-    this.filter = value
-    this.hasMore = true
+  _filter( value: string ): any{
+    this.filter = value;
+    this.hasMore = true;
 
-    if( !value ){
+    if ( !value ){
       this.users = [];
-      return false
+      return false;
     }
     this.photoService.getUserByName( this.filter )
       .pipe(debounceTime(300))
-      .subscribe(response=> {
+      .subscribe(response => {
         this.users = response;
-      })
+      });
   }
-  moreUsers(){
-    this.photoService.getUserByNamePaginated( this.filter,this.users.length )
-      .subscribe(users=> {
+  moreUsers(): void{
+    this.photoService.getUserByNamePaginated( this.filter, this.users.length )
+      .subscribe(users => {
         this.users = this.users.concat(users);
-        if(!users.length) this.hasMore = false;
-      })
+        if (!users.length){ this.hasMore = false; }
+      });
   }
-  toggleSearch(value){
+  toggleSearch(value): void{
     this._openInputSearch = value;
-    this.filter = ''
+    this.filter = '';
     this.openInputSearch.emit(this._openInputSearch);
   }
 }
