0142e00658f7a8b1f3e49d958b35fefb6ecb6960
luanmattos <patrickluan.matos@gmail.com>
2020-12-07 14:57:35 -0300

    XGH para resolver problema do xml no Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
-import {RouterOutlet} from "@angular/router";
-import {slideInAnimation} from "./core/ux/animations";
-import {Title} from "@angular/platform-browser";
+import {RouterOutlet} from '@angular/router';
+import {slideInAnimation} from './core/ux/animations';
+import {Title} from '@angular/platform-browser';
 import {Observable} from 'rxjs';
 import {SpinnerService} from './shared/spinner/spinner.service';
 
@@ -17,8 +17,8 @@ export class AppComponent{
   isSpinnerVisibile$: Observable<boolean> = this.spinnerService.isNavigationPending$;
 
   constructor(
-    private spinnerService:SpinnerService,
-    private titleService:Title) {}
+    private spinnerService: SpinnerService,
+    private titleService: Title) {}
 
   prepareRoute(outlet: RouterOutlet) {
       (outlet.activatedRouteData.title  && this.titleService.setTitle(outlet.activatedRouteData.title))
