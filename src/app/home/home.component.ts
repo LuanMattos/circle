d1dee63bd02945e066d5c523953902f45f4e039a
luanmattos <patrickluan.matos@gmail.com>
2020-12-12 19:08:45 -0300

    save request audit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,15 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from '@angular/core';
+import {LogService} from '../core/log/log.service';
 
 @Component({
-  templateUrl:'home.component.html'
+  templateUrl: 'home.component.html'
 })
-export class HomeComponent{}
+export class HomeComponent implements OnInit{
+  constructor(
+    private logService: LogService
+  ) {
+  }
+  ngOnInit(): void {
+    this.logService.logHome();
+  }
+}
