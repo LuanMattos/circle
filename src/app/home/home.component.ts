790527988449e9f024aa024964f13930c717189d
luanmattos <patrickluan.matos@gmail.com>
2021-01-04 10:53:51 -0300

    Mudança home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {LogService} from '../core/log/log.service';
 
 @Component({
-  templateUrl: 'home.component.html'
+  templateUrl: 'home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit{
   constructor(
