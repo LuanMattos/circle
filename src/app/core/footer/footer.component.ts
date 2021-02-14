914a10e2be2f51c453f70796bc24b054c41201dd
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-02-14 12:15:56 -0300

    correção rota menu service

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {UserService} from '../user/user.service';
 import {Observable} from 'rxjs';
 import {User} from '../user/user';
+import {HeaderService} from '../header/header.service';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-footer',
@@ -10,9 +12,19 @@ import {User} from '../user/user';
 export class FooterComponent implements OnInit{
 
   user$: Observable<User>;
-  constructor(private userService: UserService) {}
+  showFillerLogged;
+  constructor(
+    private userService: UserService,
+    public headerService: HeaderService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.user$ = this.userService.getUserByToken();
+  }
+  closeDialog(): void {
+    this.showFillerLogged = false;
+    this.headerService.setCurrentSession('');
+    this.dialog.closeAll();
   }
 }
