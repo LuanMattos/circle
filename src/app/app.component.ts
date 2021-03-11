c51cafcec940c54da1eac8ec6b5eae91fd2d1766
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-03-11 10:55:41 -0300

    add apache cordova to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {slideInAnimation} from './core/ux/animations';
 import {Title} from '@angular/platform-browser';
 import {Observable} from 'rxjs';
 import {SpinnerService} from './shared/spinner/spinner.service';
-
+declare var device;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,16 +13,21 @@ import {SpinnerService} from './shared/spinner/spinner.service';
     slideInAnimation
   ]
 })
-export class AppComponent{
+export class AppComponent implements OnInit {
   isSpinnerVisibile$: Observable<boolean> = this.spinnerService.isNavigationPending$;
 
   constructor(
     private spinnerService: SpinnerService,
-    private titleService: Title) {}
+    private titleService: Title) {
+  }
+
+  ngOnInit(): void {
+    document.addEventListener('deviceready', () => alert( device.platform ) );
+  }
 
   prepareRoute(outlet: RouterOutlet) {
-      (outlet.activatedRouteData.title  && this.titleService.setTitle(outlet.activatedRouteData.title))
-      return outlet && outlet.activatedRouteData
+    (outlet.activatedRouteData.title && this.titleService.setTitle(outlet.activatedRouteData.title));
+    return outlet && outlet.activatedRouteData
       && outlet.activatedRouteData.animation;
   }
 }
