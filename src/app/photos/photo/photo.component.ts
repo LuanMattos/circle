212acd319070145f8033ff2a0e8fe9637f00b55a
patrick_mattos <patrick_mattos@terceiros.sicredi.com.br>
2021-06-06 19:23:44 -0300

    Criado front end para leitura de tela - falta api que será feito com banco monngo

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,6 +1,7 @@
-import {Component, Input} from '@angular/core';
+import {AfterViewInit, Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {environment} from '../../../environments/environment';
 import {PhotoService} from './photo.service';
+
 
 const CLOUD = environment.ApiUrl + '/storage/img/';
 
@@ -9,10 +10,10 @@ const CLOUD = environment.ApiUrl + '/storage/img/';
   templateUrl: 'photo.component.html',
   styleUrls: ['./photo.scss'],
 })
-export class PhotoComponent{
+export class PhotoComponent implements AfterViewInit{
   constructor(
     private photoService: PhotoService
-  ) { }
+  ) {}
   @Input() description = '';
   @Input() _url = '';
   @Input() photo_id;
@@ -27,9 +28,36 @@ export class PhotoComponent{
   get url(): string{
     return this._url;
   }
+
+  itemsTime = [];
+
+  ngAfterViewInit(): void{
+      const id = this.photo_id;
+
+      const millisToMinutesAndSeconds = (millis) => {
+        return ((millis % 60000) / 1000).toFixed(0);
+      };
+      const observador = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          const {isIntersecting, intersectionRatio, time} = entry;
+          if (isIntersecting === true || intersectionRatio > 0) {
+            console.log( 'a photo ' + id + ' está visivel e ficou exposta por ' + millisToMinutesAndSeconds(time) + ' segundos');
+            this.sendStatistic(id);
+
+            observador.unobserve(entry.target);
+            this.sendStatistic(id);
+          }
+        });
+      }, {
+        rootMargin: '0px'
+      });
+      observador.observe(  document.querySelector('.image-' + id));
+  }
+  sendStatistic(photoId): any{
+    console.log('enviado o ' + photoId);
+  }
   errorHandler(event): any {
     event.target.src = 'https://be.mycircle.click/storage/default/error-photo.png';
     this.photoService.registerErrorPhoto(this.photo_id).subscribe();
   }
-
 }
